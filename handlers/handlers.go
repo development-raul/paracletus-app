0b9c0cb699b61377f28a6b8c2e72bc8ea4f9b3ab
Raul Brindus <raulbrindus@raul-mac.local>
2022-12-10 14:25:58 +0000

    section 5 - sessions

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/CloudyKit/jet/v6"
 	"github.com/development-raul/paracletus"
 	"net/http"
 )
@@ -12,6 +13,36 @@ type Handlers struct {
 func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
 	err := h.App.Render.Page(w, r, "home", nil, nil)
 	if err != nil {
-		h.App.ErrorLog.Println("error rendering home :", err)
+		h.App.ErrorLog.Println("error rendering home:", err)
+	}
+}
+
+func (h *Handlers) GoPage(w http.ResponseWriter, r *http.Request) {
+	err := h.App.Render.GoPage(w, r, "home", nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering go home:", err)
+	}
+}
+
+func (h *Handlers) JetPage(w http.ResponseWriter, r *http.Request) {
+	err := h.App.Render.JetPage(w, r, "jet-template", nil, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering jet-template:", err)
+	}
+}
+
+func (h *Handlers) SessionTest(w http.ResponseWriter, r *http.Request) {
+	myData := "bar"
+
+	h.App.Session.Put(r.Context(), "foo", myData)
+
+	myValue := h.App.Session.GetString(r.Context(), "foo")
+
+	vars := make(jet.VarMap)
+	vars.Set("foo", myValue)
+
+	err := h.App.Render.JetPage(w, r, "sessions", vars, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering sessions:", err)
 	}
 }
